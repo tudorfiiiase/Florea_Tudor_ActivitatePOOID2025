18d4ed03fa8de2b6bdf9b7d87bc78de72531999b
tudorfiiiase <floreatudor23@stud.ase.ro>
2025-10-18 13:00:56 +0300

    Add files via upload

---
+++ Suma.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+// Funcția suma cu doi parametri
+int suma(int a, int b) {
+    return a + b;
+}
+
+// Funcția suma cu trei parametri
+int suma(int a, int b, int c) {
+    return a + b + c;
+}
+
+int main() {
+    // Apelăm funcția suma cu doi parametri
+    int rezultat1 = suma(3, 9);
+    std::cout << "Suma a doua numere: " << rezultat1 << std::endl;
+
+    // Apelăm funcția suma cu trei parametri
+    int rezultat2 = suma(3, 5, 9);
+    std::cout << "Suma a trei numere: " << rezultat2 << std::endl;
+
+    return 0;
+}
