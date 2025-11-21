7406e3fe17afb63743301e7e449cff5d6fc1f80a
tudorfiiiase <floreatudor23@stud.ase.ro>
2025-11-21 23:27:30 +0200

    Add files via upload

---
+++ Tipuri de date.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int varsta = 22;       
+    float inaltime = 1.93f; 
+    char initiala = 'T';   
+
+    cout << "Varsta: " << varsta << endl;
+    cout << "Inaltime: " << inaltime << endl;
+    cout << "Initiala: " << initiala << endl;
+
+    return 0;
+}
