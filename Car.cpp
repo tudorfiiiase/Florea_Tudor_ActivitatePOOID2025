959773211840fb8ed199828a8f64769ffaae7f6c
tudorfiiiase <floreatudor23@stud.ase.ro>
2025-11-21 23:25:03 +0200

    Add files via upload

---
+++ Car.cpp
@@ -0,0 +1,15 @@
+﻿//Un destructor este o metodă specială a unei clase care este apelată automat atunci când obiectul iese din sfera de acțiune sau este șters.
+
+#include <iostream>
+using namespace std;
+class Car {
+public:
+    Car() {
+        //Initializari
+    }
+
+    ~Car() {
+        //Eliberari sau stergeri
+        cout << "Destructorul clasei Car a fost apelat!" << endl;
+    }
+};
