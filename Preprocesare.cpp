70f0350198e3caee2d757c575ea0d713aae70842
tudorfiiiase <floreatudor23@stud.ase.ro>
2025-12-13 20:56:39 +0200

    Add files via upload

---
+++ Preprocesare.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+
+int main() {
+    const float PI = 3.14159f; // Definim o constantă PI
+    float raza = 5.0f;         // Raza cercului
+    float aria = PI * raza * raza;
+
+    cout << "Aria unui cerc cu raza de " << raza << " este: " << aria << endl;
+
+    return 0;
+}
