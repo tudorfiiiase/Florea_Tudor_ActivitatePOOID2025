00839eec5af38ce355359e4b58a2362ec35fb1b2
tudorfiiiase <floreatudor23@stud.ase.ro>
2025-12-13 20:55:35 +0200

    Add files via upload

---
+++ Outer.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+using namespace std;
+
+class Outer {
+public:
+    // Clasa Nested definită în interiorul clasei Outer
+    class Nested {
+    private:
+        int nestedValue;
+
+    public:
+        // Constructor pentru Nested
+        Nested(int value) : nestedValue(value) {}
+
+        int getValue() const {
+            return nestedValue;
+        }
+    };
+
+    // Metodă din Outer care accesează un obiect Nested
+    void accessNested() {
+        Nested nestedObj(42); 
+        cout << "Valoarea din Nested este: " << nestedObj.getValue() << endl;
+    }
+};
+
+int main() {
+    Outer outerObj;
+    outerObj.accessNested();
+    // Alternativ, creăm direct un obiect Nested din Outer
+    Outer::Nested directNested(99);
+    cout << "Valoarea direct accesata din Nested este: " << directNested.getValue() << endl;
+    return 0;
+}
